90fe7d8d6c1f13cc9e36feff4e03d3b6c5bab2ce
ZeyadAlQutaifan <zeyada.alqutaifan@gmail.com>
2022-04-23 11:18:01 -0700

    23/4/2022

--- app/src/main/java/screens/MainActivity.java
+++ app/src/main/java/screens/MainActivity.java
@@ -1,14 +1,27 @@
-package com.example.tabib;
+package screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.example.tabib.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void toLogin(View view) {
+        Intent i = new Intent(MainActivity.this , LoginActivity.class);
+        startActivity(i);
     }
 }
