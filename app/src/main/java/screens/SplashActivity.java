155bbec372b06bbf00dd093eb1dda9dbcc25cba8
ZeyadAlQutaifan <zeyada.alqutaifan@gmail.com>
2022-04-26 07:03:50 -0700

    26/4/2022

--- app/src/main/java/screens/SplashActivity.java
+++ app/src/main/java/screens/SplashActivity.java
@@ -5,23 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
 
 import com.example.tabib.R;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 
 
 
     public void toLogin(View view) {
-        Intent i = new Intent(MainActivity.this , LoginActivity.class);
+        Intent i = new Intent(SplashActivity.this , LoginActivity.class);
         startActivity(i);
     }
 }
